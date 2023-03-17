dea8d2e70394786a380caf561b4711540c9494b6
Thomas PEN <thomas.pen@epitech.eu>
2023-03-17 20:28:57 +0100

    Fourth commit

--- myApp.js
+++ myApp.js
@@ -1,13 +1,12 @@
 let express = require('express');
 let app = express();
 
-app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
     let absolutePath = __dirname + '/views/index.html'
     res.sendFile(absolutePath)
 });
-
 
 
 
