df0e38881e1e8c34ab80f03b5469a9cb02e91305
Thomas PEN <thomas.pen@epitech.eu>
2023-03-17 20:59:07 +0100

    Seventh commit

--- myApp.js
+++ myApp.js
@@ -2,7 +2,6 @@ let express = require('express');
 let app = express();
 
 app.use("/public", express.static(__dirname + "/public"));
-
 app.get("/", (req, res) => {
     let absolutePath = __dirname + '/views/index.html'
     res.sendFile(absolutePath)
@@ -10,7 +9,9 @@ app.get("/", (req, res) => {
 
 app.get("/json", (req,res) => {
     let message = "Hello json";
-    if (process.env.MESSAGE_STYLE == "uppercase") {
+    console.log(process.env.MESSAGE_STYLE);
+
+    if (process.env.MESSAGE_STYLE === "uppercase") {
         message = message.toUpperCase();
     }
     res.json({"message": message});
