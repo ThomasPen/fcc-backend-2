221c41473d1c828c655464c1e2ec110bf6012cf9
Thomas PEN <thomas.pen@epitech.eu>
2023-03-18 01:08:04 +0100

    Eigth commit

--- myApp.js
+++ myApp.js
@@ -2,11 +2,12 @@ let express = require('express');
 let app = express();
 let dotenv = require('dotenv').config();
 
-app.use("/public", express.static(__dirname + "/public"));
 app.use((req, res, next) => {
     console.log(req.method + " " + req.path + " - " + req.ip)
     next();
 })
+app.use("/public", express.static(__dirname + "/public"));
+
 
 app.get("/", (req, res) => {
     let absolutePath = __dirname + '/views/index.html'
